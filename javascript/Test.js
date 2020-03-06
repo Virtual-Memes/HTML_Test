ae4d930ceaa63ab35fa6a85121ad1cd1fbfdf639
Virtual-Memes <549604608@qq.com>
2020-03-06 16:13:43 +0800

    updated

--- javascript/Test.js
+++ javascript/Test.js
@@ -1,5 +1,2 @@
-window.onload()=function(){
-    var B1=document.getElementById("c");
-    B1.onclick=click();
-    B1.addEventListener('click',function(){alert("1111");},true);
-}
+var B1=document.getElementById("c");
+B1.addEventListener('click',function(){alert("1111");},true);

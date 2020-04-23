4be37d736cdefe4758cc35d7a35ac4429235ceab
Virtual-Memes <549604608@qq.com>
2020-04-23 14:16:10 +0800

    updated

--- JavaScript/Test.js
+++ JavaScript/Test.js
@@ -1,14 +1,4 @@
-var jq=document.createElement("script");
-jq.type="text/Javascript";
-jq.src="./Jquery/jquery.js"
-document.getElementsByTagName('head')[0].appendChild(jq);
-setTimeout(function(){
-    $(document).ready(function(){
-        $("#b1").click(function(){
-            alert("123");
-        });
-    });
-},100);
-function b1() {
-    alert("111");
-}
+var Bt=document.getElementById("Bt1");
+Bt.addEventListener("click",function(){
+    window.location.href="./html/随机花名册.html"
+})
